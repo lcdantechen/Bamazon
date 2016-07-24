666db8277c7d682aa7b87f0860c6d8c5871425e3
Dante <lcdantechen@gmail.com>
2016-07-23 23:31:48 -0400

    week-12-Node.js & MySQL

---
+++ BamazonCustomer.js
@@ -0,0 +1,109 @@
+//INITIALIZES THE NPM PACKAGES USED//
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+
+//INITIALIZES THE CONNECTION VARIABLE TO SYNC WITH A MYSQL DATABASE//
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root", //Your username//
+    password: "", //Your password//
+    database: "Bamazon"
+})
+
+//CREATES THE CONNECTION WITH THE SERVER AND MAKES THE TABLE UPON SUCCESSFUL CONNECTION//
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+    }
+    makeTable();
+})
+
+//FUNCTION TO GRAB THE PRODUCTS TABLE FROM THE DATABASE AND PRINT RESULTS TO CONSOLE//
+var makeTable = function() {
+    //SELECTS ALL OF THE DATA FROM THE MYSQL PRODUCTS TABLE - SELECT COMMAND!
+    connection.query('SELECT * FROM products', function(err, res) {
+        if (err) throw err;
+        //PRINTS THE TABLE TO THE CONSOLE WITH MINIMAL STYLING//
+        var tab = "\t";
+        console.log("ItemID\tProduct Name\tDepartment Name\tPrice\t# In Stock");
+        console.log("--------------------------------------------------------");
+        //FOR LOOP GOES THROUGH THE MYSQL TABLE AND PRINTS EACH INDIVIDUAL ROW ON A NEW LINE//
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].ItemID + tab + res[i].ProductName + tab + res[i].DepartmentName + tab + res[i].Price + tab + res[i].StockQuantity);
+        }
+        console.log("--------------------------------------------------------");
+        //RUNS THE CUSTOMER'S PROMPTS AFTER CREATING THE TABLE. SENDS res SO THE promptCustomer FUNCTION IS ABLE TO SEARCH THROUGH THE DATA//
+        promptCustomer(res);
+    });
+};
+
+//FUNCTION CONTAINING ALL CUSTOMER PROMPTS//
+var promptCustomer = function(res) {
+        //PROMPTS USER FOR WHAT THEY WOULD LIKE TO PURCHASE//
+        inquirer.prompt([
+        {
+            type: 'input',
+            name: 'choice',
+            message: 'What would you like to purchase?'
+        }
+        ]).then(function(val) {
+
+                //SET THE VAR correct TO FALSE SO AS TO MAKE SURE THE USER INPUTS A VALID PRODUCT NAME//
+                var correct = false;
+                var record;
+                
+                //LOOPS THROUGH THE MYSQL TABLE TO CHECK THAT THE PRODUCT THEY WANTED EXISTS//
+                for (var i = 0; i < res.length; i++) {  
+                                        	
+	                //1. TODO: IF THE PRODUCT EXISTS, SET correct = true and ASK THE USER TO SEE HOW MANY OF THE PRODUCT THEY WOULD LIKE TO BUY//
+	                if (val.choice == res[i].ProductName || val.choice == res[i].ProductName.replace(/\s+/g, '').toLowerCase()) {
+                        console.log("the product you pick is"+ val.choice);
+                        correct = true;
+                        record = res[i];
+
+                    }
+                   	//2. TODO: CHECK TO SEE IF THE AMOUNT REQUESTED IS LESS THAN THE AMOUNT THAT IS AVAILABLE//                       
+	                //3. TODO: UPDATE THE MYSQL TO REDUCE THE StockQuanaity by the THE AMOUNT REQUESTED  - UPDATE COMMAND!
+	                //4. TODO: SHOW THE TABLE again by calling the function that makes the table
+                }
+
+
+                if (correct) {
+                    inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'quantity',
+                        message: 'How many would you like to purchase?'
+                    }
+                    ]).then(function(qua) {
+                        console.log(qua.quantity);                    
+                        if (qua.quantity > record.StockQuantity) {
+                            console.log("Sorry we don't have enough inventory!")
+                            console.log("Would you like to buy something else?")
+                            promptCustomer(res);
+                        } else {
+                            console.log('we have enough!');
+                            connection.query('SELECT * FROM products', function(err, res) {
+                            if (err) throw err;
+                            record.StockQuantity = record.StockQuantity - qua.quantity;
+                           /* console.log(record.StockQuantity)*/
+                           console.log("The total cost of your purchase is" + " $" + record.Price * qua.quantity);
+                            connection.query('UPDATE products SET StockQuantity = ? WHERE ItemID = ? ', [record.StockQuantity, record.ItemID])
+                            makeTable();
+
+                        
+                            });
+                            //mysql query 
+                            //in your clallback
+
+                        }
+                    });
+                } else {
+                    console.log('pLease enter a valid item');
+                    promptCustomer(res);
+                }
+            });
+}
+
+
---
+++ schema.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products(
+  ItemID INT (10) NOT NULL,
+    ProductName VARCHAR(100) NOT NULL,
+    DepartmentName VARCHAR(100) NOT NULL,
+    Price DECIMAL(10,2) NOT NULL,
+    StockQuantity INT(10) NOT NULL,
+    primary key(ItemID)
+);
+
+select * from products;
+
+Insert into products(
+    ItemID,
+    ProductName,
+    DepartmentName,
+    Price,
+    StockQuantity)
+Values (0001, "Collagen","Food Ingredient",8.75,2300),
+    (0002, "DETDA","Chemicals",89.22,1000),
+    (0003, "Silica Gel","Chemicals",12.50,50000),
+    (0004, "Fish Lures","Fishing accesorries",12.00,450),
+    (0005, "Epoxy Resins","Chemicals",24.05,150),
+    (0006, "Superman","Movie",12.02,35),
+    (0007, "Rods and Rigs","Fishing accesorries",225.10,87),
+    (0008, "Spider Man","Movie",2.50,100),
+    (0009, "Gelatin","Food Ingredient",22.10,1500),
+    (0010, "Sugar","Food Ingredient",2.22,990)
+
+
+
+UPDATE products SET StockQuantity = 2300 WHERE ProductName = "Collagen"
